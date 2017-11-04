d1011017e2cedca63232132ba1167eb0eb377ab7
Sasha Popov <Sasha@Sashas-MacBook-Pro.local>
2017-11-03 23:30:48 -0400

    training data now writes to file more efficiently

--- CartPoleTrain.py
+++ CartPoleTrain.py
@@ -65,10 +65,4 @@ print("Average score in training data set is ", np.mean(scores_from_simulations)
 print("Median score in training data set is ", np.median(scores_from_simulations))
 
 
-with open("TrainingData.txt", "w") as file:
-
-    file.write(x_training_data)
-    file.write("break")
-    file.write(y_training_data)
-
-    file.close()
+np.savez("TrainingData.csv", x_data=x_training_data, y_data=y_training_data)
